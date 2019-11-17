9b9dd9c488cb92e885fa41d269adb75ae4aaf243
Elmeri Santala <Elmeri@Elmeri-MacBook.wlan>
2019-11-17 14:59:29 +0200

    full wrnagling for exercise 3

--- data/create_alc.R
+++ data/create_alc.R
@@ -1,3 +1,112 @@
 # "Elmeri Santala"
 # "17.11.2019"
 # "Logistic regression"
+
+url <- "http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets"
+
+# web address for math class data
+url_math <- paste(url, "student-mat.csv", sep = "/")
+
+# print out the address
+url_math
+
+# read the math class questionaire data into memory
+math <- read.table(url_math, sep = ";" , header=TRUE)
+
+# web address for portuguese class data
+url_por <- paste(url, "student-por.csv", sep ="/")
+
+# print out the address
+url_por
+
+# read the portuguese class questionaire data into memory
+por <- read.table(url_por, sep = ";", header = TRUE)
+
+# look at the column names of both data
+colnames(math)
+colnames(por)
+
+# structure and dimensions of the datas
+str(por)
+dim(por)
+str(math)
+dim(math)
+
+# access the dplyr library
+library(dplyr)
+
+# common columns to use as identifiers
+join_by <- c("school","sex","age","address","famsize","Pstatus","Medu","Fedu","Mjob","Fjob","reason","nursery","internet")
+
+# join the two datasets by the selected identifiers
+math_por <- inner_join(math, por, by = join_by, suffix = c(".math", ".por"))
+
+# see the new column names
+colnames(math_por)
+
+# glimpse at the data
+glimpse(math_por)
+
+# structure and dimensions of the datas 
+str(math_por)
+dim(math_por)
+
+# print out the column names of 'math_por'
+colnames(math_por)
+
+# create a new data frame with only the joined columns
+alc <- select(math_por, one_of(join_by))
+
+# columns that were not used for joining the data
+notjoined_columns <- colnames(math)[!colnames(math) %in% join_by]
+
+# print out the columns not used for joining
+notjoined_columns
+
+# for every column name not used for joining...
+for(column_name in notjoined_columns) {
+  # select two columns from 'math_por' with the same original name
+  two_columns <- select(math_por, starts_with(column_name))
+  # select the first column vector of those two columns
+  first_column <- select(two_columns, 1)[[1]]
+  
+  # if that first column  vector is numeric...
+  if(is.numeric(first_column)) {
+    # take a rounded average of each row of the two columns and
+    # add the resulting vector to the alc data frame
+    alc[column_name] <- round(rowMeans(two_columns))
+  } else { # else if it's not numeric...
+    # add the first column vector to the alc data frame
+    alc[column_name] <- first_column
+  }
+}
+
+# glimpse at the new combined data
+glimpse(alc)
+
+# access the 'tidyverse' packages dplyr and ggplot2
+library(dplyr); library(ggplot2)
+
+# define a new column alc_use by combining weekday and weekend alcohol use
+alc <- mutate(alc, alc_use = (Dalc + Walc) / 2)
+
+# initialize a plot of alcohol use
+g1 <- ggplot(data = alc, aes(x = alc_use, fill = sex))
+
+# define the plot as a bar plot and draw it
+g1 + geom_bar()
+
+# define a new logical column 'high_use'
+alc <- mutate(alc, high_use = alc_use > 2)
+
+# initialize a plot of 'high_use'
+g2 <- ggplot(alc, aes(high_use))
+
+# draw a bar plot of high_use by sex
+g2 + facet_wrap("sex") + geom_bar()
+
+# glimpse at the new combined data
+glimpse(alc)
+
+# save data
+write.csv(alc, file = "data/alc.csv", row.names=FALSE)
